f493241f15e40f1bb915dbe55ea2b506929d2298
Bacarus <code.bacarus@gmail.com>
2021-04-15 19:30:38 +0200

    exercise: generics and accumulator example implementation

--- PDS_Generics/main.cpp
+++ PDS_Generics/main.cpp
@@ -3,42 +3,51 @@ template <typename T>
 const T& max(const T& t1, const T& t2);
 class Person{
 public:
-    int id;
-    static int counter;
-    int getId(){ return id;}
-    Person(){ counter++;id = counter;}
-private:
     std::string name;
     std::string surname;
+    int age;
+    Person(std::string name, std::string surname, int age) :
+        name(name), surname(surname), age(age) { };
+
+    Person& operator+=(const Person& other){
+        this->age+=other.age;
+        return *this;
+    }
+
 };
 template <typename T>
 class Accum{
     T total;
 public:
-    Accum(T start): total(start){}
+    Accum(T start = 0): total(start){}
     T operator+=(const T& t){
         return total = total+t;
     }
     T value(){return total;}
 };
-template class Accum<Person>{
-    int total;
-public:
-    Accum(int start = 0): total(start){};
-    int operator+=(const Person &){return ++total;}
-    int value(){return total;}
 
-};
 
-int Person::counter;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     int maxNumber = max(10,5);
+//    int maxNumber = max(2,3.14); // compile error because the types are different
     std::cout << "This is the max between 10 and 5 : " << maxNumber << std::endl;
-    Person p1 = Person();
-    Person* p2 = new Person();
-    std::cout << "id of p1 and p2" << std::endl;
+    float maxFloat = max<float>(2,3.14);
+    std::cout << "This is the max between 2 and 3.14 : " << maxFloat << std::endl;
 
+    Accum<int> a1(0);
+    for(int i = 0; i<10; i++) a1+=i;
+    std::cout << "Accumulated value is : " << a1.value() << std::endl;
+    Accum<std::string> a2("");
+    std::string s{"*"};
+    for(int i = 0; i<10; i++) a2+=s;
+    std::cout << "Accumulated value is : " << a2.value() << std::endl;
+
+
+    Person p1 = Person("Eikichi","Onizuka",22);
+    Person* p2 = new Person("Mew","Burzi",7);
+    p1+=*p2;
+    std::cout << "Gesu age + Burzi age = " << p1.age << std::endl;
     delete p2;
     return 0;
 }
